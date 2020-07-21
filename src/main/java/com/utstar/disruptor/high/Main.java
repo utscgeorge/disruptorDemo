cb131a58389f5482988aab8dccc61af0d63e7b1c
george.zhang <george.zhang@utstarcom.cn>
2020-07-21 23:03:24 +0800

    init

--- src/main/java/com/utstar/disruptor/high/Main.java
+++ src/main/java/com/utstar/disruptor/high/Main.java
@@ -21,7 +21,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         ExecutorService es1 = Executors.newFixedThreadPool(4);
-        ExecutorService es2 = Executors.newFixedThreadPool(4);
+        ExecutorService es2 = Executors.newFixedThreadPool(5);
 
         Disruptor<Trade> disruptor = new Disruptor<>(new EventFactory<Trade>() {
             @Override
@@ -43,9 +43,29 @@ public class Main {
         disruptor.handleEventsWith(new Handler2());
         disruptor.handleEventsWith(new Handler3());*/
 
-        //多边形
-        EventHandlerGroup<Trade> tradeEventHandlerGroup = disruptor.handleEventsWith(new Handler1(), new Handler2());
-        tradeEventHandlerGroup.then(new Handler3());
+        //菱形
+        /*EventHandlerGroup<Trade> tradeEventHandlerGroup = disruptor.handleEventsWith(new Handler1(), new Handler2());
+        tradeEventHandlerGroup.then(new Handler3());*/
+
+        //六边形:disruptor中线程数量要和handler数量大于或等于之
+        /**
+         *
+         *       h1------>h2
+         * s                        h3
+         *       h4------>h5
+         */
+        Handler1 h1 = new Handler1();
+        Handler2 h2 = new Handler2();
+        Handler3 h3 = new Handler3();
+        Handler4 h4 = new Handler4();
+        Handler5 h5 = new Handler5();
+        disruptor.handleEventsWith(h1,h4);
+        disruptor.after(h1).handleEventsWith(h2);
+        disruptor.after(h4).handleEventsWith(h5);
+        disruptor.after(h2,h5).handleEventsWith(h3);
+
+
+
 
         RingBuffer<Trade> ringBuffer = disruptor.start();
 
@@ -59,7 +79,7 @@ public class Main {
         disruptor.shutdown();
         es1.shutdown();
         es2.shutdown();
-
+        System.out.println("处理完成!!");
 
     }
 }
